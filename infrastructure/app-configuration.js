0a19d952c7c3aa6923981519e2788d73c88315ec
Stawicki <stawickipiotr94@gmail.com>
2018-01-31 13:53:52 -0800

    Fix EUREKA_APP_HOSTNAME variable

--- infrastructure/app-configuration.js
+++ infrastructure/app-configuration.js
@@ -15,9 +15,9 @@ module.exports = {
             MENU_SERVICE_URL: 'http://iosr2017menu-staging.herokuapp.com',
             STORAGE_SERVICE_URL: 'http://iosr2017storage-staging.herokuapp.com',
             CLOUDAMQP_URL: 'amqp://grponzdp:pwcTF2Rql4tGh3SyssbVOxe2Ih6jTFJb@gopher.rmq.cloudamqp.com/grponzdp',
-            EUREKA_APP_HOST_NAME: `http://${HEROKU_APP_NAME}-staging.herokuapp.com`,
+            EUREKA_APP_HOST_NAME: `${HEROKU_APP_NAME}-staging.herokuapp.com`,
             EUREKA_APP_IP_ADDR: '127.0.0.1',
-            EUREKA_APP_VIP_ADDR: `http://${HEROKU_APP_NAME}-staging.herokuapp.com`,
+            EUREKA_APP_VIP_ADDR: `${HEROKU_APP_NAME}-staging.herokuapp.com`,
             EUREKA_SERVER_URL: 'https://user:iosr2017@iosr2017eureka-staging.herokuapp.com/eureka/apps'
         },
         addons: {
@@ -48,9 +48,9 @@ module.exports = {
             MENU_SERVICE_URL: 'http://iosr2017menu-production.herokuapp.com',
             STORAGE_SERVICE_URL: 'http://iosr2017storage-production.herokuapp.com',
             CLOUDAMQP_URL: 'amqp://grponzdp:pwcTF2Rql4tGh3SyssbVOxe2Ih6jTFJb@gopher.rmq.cloudamqp.com/grponzdp',
-            EUREKA_APP_HOST_NAME: `http://${HEROKU_APP_NAME}-production.herokuapp.com`,
+            EUREKA_APP_HOST_NAME: `${HEROKU_APP_NAME}-production.herokuapp.com`,
             EUREKA_APP_IP_ADDR: '127.0.0.1',
-            EUREKA_APP_VIP_ADDR: `http://${HEROKU_APP_NAME}-production.herokuapp.com`,
+            EUREKA_APP_VIP_ADDR: `${HEROKU_APP_NAME}-production.herokuapp.com`,
             EUREKA_SERVER_URL: 'https://user:iosr2017@iosr2017eureka-production.herokuapp.com/eureka/apps'
         },
         addons: {
